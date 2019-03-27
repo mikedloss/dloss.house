8b19edef6c76b33651964cc56e206c2bc97b4753
Mike DLoss <Mike.DLoss@lcecorp.com>
2019-03-27 10:46:52 -0400

    fix layout container width

--- src/components/Layout/Layout.styles.js
+++ src/components/Layout/Layout.styles.js
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 import { Box } from 'rebass';
 
-export const SiteContainer = styled(Box)`
+export const ContentContainer = styled(Box)`
   margin: 0 auto;
   max-width: 1600px;
-`;
-
-export const ContentContainer = styled(Box)`
   padding: 1rem;
 `;
