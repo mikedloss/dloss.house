8411aadaacd81dafa7d8990336c8853bd9220928
Mike DLoss <mike.dloss@gmail.com>
2019-07-30 21:21:26 -0400

    removed sharp things

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: 'gatsby-source-contentful',
       options: {
