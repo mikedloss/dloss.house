6421e706556372f656c453789f2ade09b099d365
Mike DLoss <mike@brushfire.com>
2019-11-02 21:40:58 -0400

    remove styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-emotion',
-    // {
-    //   resolve: 'gatsby-plugin-styled-components',
-    //   options: {
-    //     displayName: isDev,
-    //   },
-    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
