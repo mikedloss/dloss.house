bde167fd115f189e1d37f1c17c9b69e16b492653
Mike DLoss <mike.dloss@gmail.com>
2020-11-10 13:01:04 -0500

    add GameProfile page (/boardgames/[id])

---
+++ components/GameProfile/GameProfile.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Box, Button, Collapse, Flex, Heading, Image, Link, Text, useDisclosure } from '@chakra-ui/core';
+import { BoardGame } from '../../lib/models/Game';
+import { DifficultyBadge } from '../DifficultyBadge';
+
+interface GamePropertyProps {
+  label: string;
+  value?: string | number;
+}
+
+const GameProperty: React.FC<GamePropertyProps> = ({ label, value, children }) => {
+  return (
+    <Flex flexDirection="column" marginY="0.5rem">
+      <Text as="span" fontSize="sm" fontWeight="bold">
+        {label}
+      </Text>
+      {children ? <>{children}</> : <Text fontSize="xl">{value || 'No data found'}</Text>}
+    </Flex>
+  );
+};
+
+export interface GameProfileProps {
+  game: BoardGame;
+}
+
+export const GameProfile: React.FC<GameProfileProps> = ({ game }) => {
+  const { isOpen, onToggle } = useDisclosure();
+  return (
+    <Flex flexDirection="column">
+      <Flex flexDirection="column" marginBottom="2rem">
+        <Heading>{game.title}</Heading>
+        {game.type && <Text fontSize="sm">This is a {game.type}</Text>}
+      </Flex>
+
+      <Flex flexDirection={{ base: 'column', md: 'row-reverse' }}>
+        {game.image && (
+          <Box width={{ base: '100%', md: '50%' }}>
+            <Image src={game.image} width={{ base: '300px', sm: '400px', md: '500px' }} />
+          </Box>
+        )}
+        <Flex flexDirection="column" width={{ base: '100%', md: '50%' }}>
+          <GameProperty label="BGG Rating" value={game.bggRating} />
+          <GameProperty label="BGG Rank" value={game.bggRank && `#${game.bggRank}`} />
+          <GameProperty label="Difficulty (Weight)">
+            <Flex alignItems="center">
+              <Text fontSize="xl" mr="0.5rem">
+                {game.difficulty} / 5
+              </Text>
+              <DifficultyBadge difficulty={game.difficulty} />
+            </Flex>
+          </GameProperty>
+          <GameProperty
+            label="Players"
+            value={
+              game.minPlayers && game.maxPlayers
+                ? game.minPlayers === game.maxPlayers
+                  ? game.minPlayers
+                  : `${game.minPlayers}-${game.maxPlayers}`
+                : null
+            }
+          />
+          <GameProperty
+            label="Playing Time (minutes)"
+            value={
+              game.minPlayingTime && game.maxPlayingTime
+                ? game.minPlayingTime === game.maxPlayingTime
+                  ? game.minPlayingTime
+                  : `${game.minPlayingTime}-${game.maxPlayingTime}`
+                : null
+            }
+          />
+          <GameProperty label="Categories" value={game.categories ? game.categories.join(', ') : null} />
+          <GameProperty label="Mechanisms" value={game.mechanisms ? game.mechanisms.join(', ') : null} />
+          <GameProperty label="Game ID">
+            <Flex flexDirection="column" alignItems="flex-start">
+              <Text fontSize="xl">{game.bggId}</Text>
+              <Link
+                minWidth="0"
+                href={`https://boardgamegeek.com/boardgame/${game.bggId}`}
+                target="_blank"
+                rel="noreferrer noopener"
+                color="blue.500"
+              >
+                View on BGG
+              </Link>
+            </Flex>
+          </GameProperty>
+        </Flex>
+      </Flex>
+      {game.description && (
+        <GameProperty label="Description">
+          <Box>
+            <Button onClick={onToggle} size="sm">
+              Show Description
+            </Button>
+            <Collapse in={isOpen} animateOpacity>
+              <Box margin="1rem">
+                <Text>{game.description}</Text>
+              </Box>
+            </Collapse>
+          </Box>
+        </GameProperty>
+      )}
+    </Flex>
+  );
+};
--- pages/boardgames/[id].tsx
+++ pages/boardgames/[id].tsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Box, Center, Flex, Heading, Skeleton, Text } from '@chakra-ui/core';
 
-const SingleBoardGamePage: React.FC = () => {
-  return <div>hello!</div>;
+import { NotFound } from '../../components/NotFound';
+import { BoardGame } from '../../lib/models/Game';
+import { useBoardGames } from '../../components/hooks';
+import { GameProfile } from '../../components/GameProfile';
+
+export interface SingleBoardGamePageProps {
+  game: BoardGame;
+}
+
+const SingleBoardGamePage: React.FC<SingleBoardGamePageProps> = () => {
+  const router = useRouter();
+  const { games, count, isLoading, isError, error } = useBoardGames(router.query.id as string);
+
+  return (
+    <>
+      {isLoading ? (
+        <Skeleton height="20px" />
+      ) : isError || count === 0 ? (
+        <NotFound>
+          <Heading>Not found!</Heading>
+          <Text>No game was found with ID {router.query.id}</Text>
+        </NotFound>
+      ) : (
+        <>
+          {count > 1 ? <Heading>More than 1 game</Heading> : null}
+          <GameProfile game={games[0]} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default SingleBoardGamePage;
