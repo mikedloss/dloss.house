d04751efd7b05cae8166fada72b3f554dbe0cfbd
Mike DLoss <mike.dloss@gmail.com>
2020-12-05 18:25:25 -0500

    remove non graphql endpoints

--- pages/api/boardgames/all.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getAllBoardGames } from '../../../lib/contentful';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const games = await getAllBoardGames();
-  res.statusCode = 200;
-  res.json(games);
-};
--- pages/api/boardgames/search/add.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  // const games = await getAllBoardGames();
-  res.statusCode = 200;
-  res.json({});
-};
